0d4447be3514b8f67a484b46fb6f04347f5cd57b
Thiago Benedicto <benedict.thiago@gmail.com>
2024-05-09 23:50:03 -0300

    chore: postgres docker

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,12 +4,12 @@ import { UserService } from 'src/users/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor (
+  constructor(
     private userService: UserService,
     private jwtService: JwtService
-  ) {}
+  ) { }
 
-  async validateUser (login: string, password: string): Promise<any> {
+  async validateUser(login: string, password: string): Promise<any> {
     const user = await this.userService.userByEmail(login)
     if (user && user.password === password) {
       const { password, ...result } = user
@@ -21,6 +21,7 @@ export class AuthService {
   async login(user: any) {
     const payload = { login: user.login, sub: user.userId }
     return {
-    access_token: this.jwtService.sign(payload)}
+      access_token: this.jwtService.sign(payload)
+    }
   }
 }
