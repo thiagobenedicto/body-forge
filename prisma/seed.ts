ccef4e5a62401b761812b241b251b7df9ac9c33e
Thiago Benedicto <benedict.thiago@gmail.com>
2024-06-10 18:40:17 -0300

    fix: update seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,17 @@
 import { PrismaClient } from '@prisma/client'
+import * as bcrypt from 'bcrypt';
 const prisma = new PrismaClient()
 async function main() {
+  const saltRounds = 10;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(process.env.MASTER_PASSWORD, salt);
+
   const user = await prisma.users.create(
     {
       data: {
         name: "Thiago Benelirius",
         login: "thiago.ava@gmail.com",
-        password: "senha"
+        password: hash
       }
     }
   )
